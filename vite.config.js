fb96a78fc9a8ee0041096acfc3cb2546cf3d89fc
Wayne Graham <wgraham@clir.org>
2024-11-05 20:29:19 -0500

    Add base for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'bjj-study-guide',
   plugins: [react()],
   css: {
     postcss: './postcss.config.js',
